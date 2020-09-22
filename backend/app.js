e853a43eff78ea1de1e2218c25aaba3f7c70f1c3
shineunji <dmswlckato@gmail.com>
2020-09-22 23:06:25 +0900

    mern

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const Todo = require('./src/controllers/todo')
-const Health = require('./src/controllers/health')
+const dotenv = require('dotenv');
 
 dotenv.config();
-
 const app = express();
+
 mongoose.connect('mongodb+srv://koo6357:koo6357@cluster0.g50h2.mongodb.net/todosShin?retryWrites=true&w=majority')
-    .then(res => {
-        console.log('success');
-    })
-    .catch(err => {
-        console.log("err", err);
-    })
+.then (() => {
+    console.log("connect success");
+})
+.catch ((err) => {
+    return err
+})
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/todo', Todo);
-app.use('/health', Health);
+
 
 const PORT = process.env.PORT;
 
--- backend/src/controllers/todo.js
+++ backend/src/controllers/todo.js
@@ -3,10 +3,19 @@ const Todo = require('../models/todo');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const result = req.body;
-    console.log("result", result);
+router.get('/', async (req, res) => {
+    const result = await Todo.find({})
+
+    res.json(result)
 })
 
+router.post('/', async (req, res) => {
+    const {title, description} = req.body;
+
+    const todo = new Todo({title, description})
+    const result = await todo.save()
+
+    res.json(result)
+})
 
 module.exports = router;
